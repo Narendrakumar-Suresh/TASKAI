a7b64522a60b5211924cddde92e868422e14fbef
Narendrakumar-Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2024-08-12 19:25:33 +0530

    change is client

--- src/lib/helper/supabaseClient.js
+++ src/lib/helper/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Create a single supabase client for interacting with your database
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
