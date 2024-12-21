f019b4a8a6436ecab7f6313fb0dc8a79989c416a
Narendrakumar-Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2024-12-21 09:37:44 +0530

    AI bug fix

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -8,7 +8,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-exp",
+  model: "gemini-1.5-flash-8b",
 });
 
 const generationConfig = {
